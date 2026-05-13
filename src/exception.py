acff61aed1ff510d46248a77562dff9809a703b1
Missclick <gabrielgarrone670@gmail.com>
2026-05-13 14:08:10 +0200

    fix: add pass statement to DbError exception class for consistency

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,7 @@
 class ConvertError(Exception):
     """Raised when the input value is not a valid number"""
     pass
+
+class DbError(Exception):
+    """Raised when there is an error with the database connection or query"""
+    pass
